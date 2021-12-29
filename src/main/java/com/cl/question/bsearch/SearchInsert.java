cf6df56663fb0b8ceb0686d3128386eb87e64ad1
chenliang <1260328762@qq.com>
2021-12-29 21:33:22 +0800

    刷题记录

---
+++ src/main/java/com/cl/question/bsearch/SearchInsert.java
@@ -0,0 +1,44 @@
+package com.cl.question.bsearch;
+
+/**
+ * @author chenliang
+ * @since 2021/12/29 21:31
+ * <p>
+ * 35. 搜索插入位置
+ * <p>
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SearchInsert {
+
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                // 左探测
+                if (mid == 0 || nums[mid - 1] < target) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+                // 右探测
+                if (mid == nums.length - 1 || nums[mid + 1] > target) {
+                    return low;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
