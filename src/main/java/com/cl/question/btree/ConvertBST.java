b1816b841a8030dc6642cf35e86131093893b14e
chenliang <1260328762@qq.com>
2022-06-09 11:55:51 +0800

    优化

--- src/main/java/com/cl/question/btree/ConvertBST.java
+++ src/main/java/com/cl/question/btree/ConvertBST.java
@@ -4,7 +4,7 @@ package com.cl.question.btree;
  * @author chenliang
  * @since 2022/2/15 11:15
  * <p>
- * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+ * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node的新值等于原树中大于或等于 node.val 的值之和。
  * <p>
  * 提醒一下，二叉搜索树满足下列约束条件：
  * <p>
