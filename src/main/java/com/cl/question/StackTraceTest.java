f0d0a1067c71d1912b049c822cc557899565a817
chenliang <1260328762@qq.com>
2022-11-06 16:09:15 +0800

    Demo of Thread.getAllStackTraces()

---
+++ src/main/java/com/cl/question/StackTraceTest.java
@@ -0,0 +1,20 @@
+package com.cl.question;
+
+import java.util.Map;
+
+/**
+ * @author chenliang
+ * @since 2022/11/6 16:06
+ */
+public class StackTraceTest {
+    public static void main(String[] args) {
+        Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
+
+        for (Map.Entry<Thread, StackTraceElement[]> threadEntry : allStackTraces.entrySet()) {
+            System.out.println("Thread: " + threadEntry.getKey());
+            for (StackTraceElement stackTraceElement : threadEntry.getValue()) {
+                System.out.println("stackTraceElement: " + stackTraceElement);
+            }
+        }
+    }
+}
