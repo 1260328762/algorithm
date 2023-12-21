2c8beed30a26b35af967993ab9bd55071344c202
chenliang <1260328762@qq.com>
2023-12-21 18:57:18 +0800

    敏感信息去除

--- src/main/java/com/cl/network/AutoWarning.java
+++ src/main/java/com/cl/network/AutoWarning.java
@@ -16,9 +16,10 @@ public class AutoWarning {
 
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
-        String condition = "[{\"opName\":\"and\",\"opProperty\":\"\",\"sonSize\":5,\"chunkedResult\":\"市值从小到大前100个,_&_非st,_&_主板\",\"relatedSize\":0},{\"opName\":\"sort\",\"opProperty\":\"从小到大排名前100\",\"sonSize\":1,\"uiText\":\"2023年12月06日总市值从小到大排名前100\",\"queryText\":\"2023年12月06日总市值从小到大排名前100\",\"relatedSize\":1},{\"indexName\":\"总市值\",\"indexProperties\":[\"nodate 1\",\"交易日期 20231206\"],\"indexPropertiesMap\":{\"交易日期\":\"20231206\",\"nodate\":\"1\"},\"type\":\"index\",\"sonSize\":0,\"reportType\":\"TRADE_DAILY\",\"valueType\":\"_浮点型数值(元|港元|美元|英镑)\",\"domain\":\"abs_股票领域\",\"source\":\"new_parser\",\"dateType\":\"交易日期\",\"tag\":\"总市值\",\"relatedSize\":0},{\"opName\":\"and\",\"opProperty\":\"\",\"sonSize\":2,\"relatedSize\":0},{\"indexName\":\"股票简称\",\"indexProperties\":[\"不包含st\"],\"indexPropertiesMap\":{\"不包含\":\"st\"},\"type\":\"index\",\"sonSize\":0,\"reportType\":\"null\",\"valueType\":\"_股票简称\",\"domain\":\"abs_股票领域\",\"source\":\"new_parser\",\"tag\":\"股票简称\",\"uiText\":\"股票简称不包含st\",\"queryText\":\"股票简称不包含st\",\"relatedSize\":0},{\"indexName\":\"上市板块\",\"indexProperties\":[\"包含主板\"],\"indexPropertiesMap\":{\"包含\":\"主板\"},\"type\":\"index\",\"sonSize\":0,\"reportType\":\"null\",\"valueType\":\"_上市板块\",\"domain\":\"abs_股票领域\",\"source\":\"new_parser\",\"tag\":\"上市板块\",\"uiText\":\"上市板块是主板\",\"queryText\":\"上市板块是主板\",\"relatedSize\":0}]\n";
-        String w = "市值从小到大前100个，非st，主板\n";
-        String domain = "abs_股票领域";
+        String condition = "[]";
+
+        String w = "keyword\n";
+        String domain = "领域";
 
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         body.add("condition", condition);
@@ -33,24 +34,27 @@ public class AutoWarning {
 
         JSONArray jsonArray = result.getJSONObject("data")
                 .getJSONArray("data");
-        JSONObject jsonObject = jsonArray.getJSONObject(0);
 
-        body.clear();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            body.clear();
 
-        body.add("userId", "659352650");
-        body.add("limit", true);
-        body.add("channel", "THS_MOBILE");
-        body.add("stkcode", jsonObject.getString("hqCode"));
-        body.add("stkname", jsonObject.getString("股票简称"));
-        body.add("marketcode", "33");
-        body.add("warnType", "0");
-        body.add("pushInfo", "[{\"pushClient\":\"PUSH\",\"pushStatus\":1,\"pushParams\":null},{\"pushClient\":\"WX\",\"pushStatus\":1,\"pushParams\":null},{\"pushClient\":\"SMS\",\"pushStatus\":0,\"pushParams\":null}]");
-        body.add("frequency", "2");
-        body.add("warnIndexs", "[{\"condId\":59,\"indexValue\":\"\"}]");
+            body.add("userId", "用户id");
+            body.add("limit", true);
+            body.add("channel", "THS_MOBILE");
+            body.add("stkcode", jsonObject.getString("hqCode"));
+            body.add("stkname", jsonObject.getString("股票简称"));
+            body.add("marketcode", "33");
+            body.add("warnType", "0");
+            body.add("pushInfo", "推送信息");
+            body.add("frequency", "2");
+            body.add("warnIndexs", "[{\"condId\":59,\"indexValue\":\"\"}]");
 
-        // todo 添加Cookie
-        String resultString = restTemplate.postForObject("https://vaserviece.10jqka.com.cn/iwcalarm/nlp/v1/add_warn_info",
-                new HttpEntity<>(body, headers), String.class);
-        System.out.println(resultString);
+            // TODO 添加Cookie
+            String resultString = restTemplate.postForObject("预警链接",
+                    new HttpEntity<>(body, headers), String.class);
+            System.out.println(resultString);
+        }
+
     }
 }
